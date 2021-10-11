2256660fce7e7dac7d68cfcd6a788908bc8c49ac
danilasmilas@gmail.com <danilasmilas@gmail.com>
2021-10-11 10:58:06 +0300

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab1
+{
+    class Lab1
+    {
+
+
+        static void Main()
+        {
+            Student student = new Student("Danil", "Prosenko", 18, "Ukraine", "Skadovsk", "Gorkogo", 8);
+            student.GetInfo();
+
+            Teacher teacher = new Teacher("Vladislav", "Bochkarev", 35, "Ukraine", "Kherson", "Perekopskaya", 20);
+            teacher.GetInfo();
+
+            Teacherlist teacherlist = new Teacherlist();
+            teacherlist.GetInfo();
+        }
+    }
+}
---
+++ lab1/Teacherlist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    class Teacherlist
+    {
+        private List<Teacher> Teacher_list;
+
+        public Teacherlist()
+        {
+            Teacher_list = new List<Teacher>();
+        }
+
+        public void Add_Teacher(Teacher teacher)
+        {
+            Teacher_list.Insert(0, teacher);
+            Console.WriteLine("Учитель успешно добавлен");
+        }
+
+        public void Delete_Teacher(string Surname)
+        {
+            foreach (Teacher j in Teacher_list)
+            {
+                if (Surname != j.Surname)
+                {
+                    Teacher_list.Remove(j);
+                    Console.WriteLine("Учитель успешно удалён");
+                }
+            }
+        }
+
+        public void GetInfo()
+        {
+            foreach (Teacher j in Teacher_list)
+            {
+                j.GetInfo();
+            }
+        }
+
+    }
+}

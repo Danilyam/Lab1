62123155a3b59afbbe791689bad080256036cd67
Danilyam <91607331+Danilyam@users.noreply.github.com>
2021-10-27 16:15:12 +0300

    Add files via upload

---
+++ Windows Form/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private List<Human> CreateHuman()
+        {
+            List<Human> humans = new List<Human>();
+            Human pip = new Human(177, "Grigoriy", "Bochkarev", 18);
+            Human pip1 = new Human(256, "Andrew", "Mihalkov", 35);
+            Human pip2 = new Human(017, "Natasha", "Kravchenko", 19);
+            Human pip3 = new Human(100, "Sasha", "Ivanova", 25);
+
+            humans.Add(pip);
+            humans.Add(pip1);
+            humans.Add(pip2);
+            humans.Add(pip);
+            return humans;
+        }
+
+        public void InitionalDatatable()
+        {
+            List<Human> humans = CreateHuman();
+            DataTable table = new DataTable();
+            table.Columns.Add("Id");
+            table.Columns.Add("Name");
+            table.Columns.Add("Surname");
+            table.Columns.Add("Age");
+            foreach(Human a in humans)
+            {
+                table.Rows.Add(a.Id, a.Name, a.Surname, a.Age);
+            }
+            dataGridView1.DataSource = table;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitionalDatatable();
+            InitionalChart();
+        }
+
+        private void InitionalChart()
+        {
+            List<Human> humans = CreateHuman();
+            for (int i = 0; i < humans.Count; i++)
+            {
+                chart1.Series["Age"].Points.AddXY(humans[i].Name, humans[i].Age);
+            }
+        }
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Windows Form/WindowsFormsApp1/WindowsFormsApp1/Human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Human
+    {
+        private int id;
+        private string name;
+        private string surname;
+        private int age;
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public string Surname { get { return surname; } set { surname = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public Human(int Id, string Name, string Surname, int Age)
+        {
+            id = Id;
+            surname = Surname;
+            name = Name;
+            age = Age;
+        }
+
+    }
+}
